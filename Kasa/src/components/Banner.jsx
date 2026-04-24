b69b452c92a83807f36fdb6dec79b24b9cbbc20f
Camelia Guerroudj <camelia.guerroudj@gmail.com>
2026-04-24 20:41:25 +0200

    remove old Kasa project, keep front-end only

--- Kasa/src/components/Banner.jsx
+++
@@ -1,9 +0,0 @@
-const Banner = () => {
-    return (
-        <div className="banner">
-            <h1>Chez vous, partout et ailleurs</h1>
-        </div>
-    );
-};
-
-export default Banner;
--- Kasa/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <p>© 2020 Kasa. All rights reserved</p>
-        </footer>
-    );
-}
